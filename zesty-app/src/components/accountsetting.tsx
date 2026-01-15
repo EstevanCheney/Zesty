754b3ad29ce2c11cd98fb706e37484c084781cfc
Estevan Cheney <estevan.cheney@epitech.eu>
2026-01-15 12:05:52 +0100

    feat: Add the css

--- zesty-app/src/components/accountsetting.tsx
+++ zesty-app/src/components/accountsetting.tsx
@@ -14,7 +14,7 @@ import {
   SelectValue,
 } from "./ui/select";
 import { Avatar, AvatarFallback } from "./ui/avatar";
-import { toast } from "sonner@2.0.3";
+import { toast } from "sonner";
 
 interface AccountSettingsProps {
   onBack?: () => void;
